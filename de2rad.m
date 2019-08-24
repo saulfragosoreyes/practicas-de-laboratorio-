cb46040ad990c05ce95fbcf1d18b6fae57b40630
saulfragosoreyes <54422936+saulfragosoreyes@users.noreply.github.com>
2019-08-23 20:52:49 -0500

    Add files via upload

---
+++ de2rad.m
@@ -0,0 +1,3 @@
+function radOutput = de2rad(degInput)
+pi=3.1415926;
+radOutput = degInput*pi/180;
---
+++ rectangulonormal.m
@@ -0,0 +1,31 @@
+close all
+
+
+line([-1 1],[0 0],[0 0],'color',[1 0 0],'linewidth',2.5);
+line([0 0],[-1 1],[0 0],'color',[0 1 0],'linewidth',2.5);
+
+p1 = [0 0];
+p2 = [6 0];
+p3 = [6 3];
+p4 = [0 3];
+
+line([p1(1) p2(1)],[p1(2) p2(2)],[0 0],'color',[0 1 1], 'linewidth',2.5);
+line([p2(1) p3(1)],[p2(2) p3(2)],[0 0],'color',[0 1 1], 'linewidth',2.5);
+line([p3(1) p4(1)],[p3(2) p4(2)],[0 0], 'color',[0 1 1], 'linewidth',2.5);
+line([p4(1) p1(1)],[p4(2) p1(2)],[0 0], 'color',[0 1 1], 'linewidth',2.5);
+
+angle_rad=deg2rad(90);
+
+Rz= [cos(angle_rad) -sin(angle_rad); sin(angle_rad) cos(angle_rad)];
+
+p1r=Rz*p1';
+p2r=Rz*p2';
+p3r=Rz*p3';
+p4r=Rz*p4';
+
+line([p1r(1) p2r(1)],[p1r(2) p2r(2)],[0 0], 'color',[0 1 0], 'linewidth',2.5);
+line([p2r(1) p3r(1)],[p2r(2) p3r(2)],[0 0], 'color',[0 1 0], 'linewidth',2.5);
+line([p3r(1) p4r(1)],[p3r(2) p4r(2)],[0 0], 'color',[0 1 0], 'linewidth',2.5);
+line([p4r(1) p1r(1)],[p4r(2) p1r(2)],[0 0], 'color',[0 1 0], 'linewidth',2.5);
+
+pause (0.01);
